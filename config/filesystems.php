0e1fbb41b144a588ee9a648082a9e110a5f46c45
Mohamed Ismail <mismail80@gmail.com>
2026-06-21 16:58:01 +0300

    Add digital ocean spaces

--- config/filesystems.php
+++ config/filesystems.php
@@ -64,6 +64,19 @@ return [
             'throw'                   => false,
         ],
 
+        'spaces' => [
+            'driver'                  => 's3',
+            'key'                     => env('DO_SPACES_KEY'),
+            'secret'                  => env('DO_SPACES_SECRET'),
+            'region'                  => env('DO_SPACES_REGION', 'nyc3'),
+            'bucket'                  => env('DO_SPACES_BUCKET'),
+            'endpoint'                => env('DO_SPACES_ENDPOINT'),
+            'url'                     => env('DO_SPACES_URL'),
+            'use_path_style_endpoint' => false,
+            'visibility'              => 'public',
+            'throw'                   => true,
+        ],
+
     ],
 
     /*
